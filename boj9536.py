137b5da138c55b287e51872283d01a898991a3a3
sjmjys954646 <sjmjys954646@gmail.com>
2020-12-11 21:12:47 +0900

    boj9536

---
+++ boj9536.py
@@ -0,0 +1,24 @@
+testcase = int(input())
+m= {}
+for i in range(0,testcase):
+    m.clear()
+    sound = input()
+    splitsound = sound.split(' ')
+    while True:
+        order = input()
+        if order == "what does the fox say?":
+            break;
+        splitorder = order.split(' ')
+        m[splitorder[0]] = splitorder[2]
+
+    for index in m.values():
+        while index in splitsound:    
+            splitsound.remove(index) 
+
+    for p in splitsound:
+        print(p, end = ' ')
+
+
+        
+    
+
